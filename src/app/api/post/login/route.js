e6f53f85c2a1fecf79b5d0c3cfc9098f539afe5d
massylamarni <massyl.amarni@outlook.fr>
2025-09-21 17:53:00 +0200

    cors fix #3.1

--- src/app/api/post/login/route.js
+++ src/app/api/post/login/route.js
@@ -5,12 +5,12 @@ import bcrypt from "bcryptjs";
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-const corsHeaders = (req) => {
+const corsHeaders = (req) => ({
   "Access-Control-Allow-Origin": req.headers.get("origin"),
   "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
   "Access-Control-Allow-Credentials": "true",
-};
+});
 
 export async function OPTIONS(req) {
   return NextResponse.json({}, { headers: corsHeaders(req) });
--- src/app/api/post/register/route.js
+++ src/app/api/post/register/route.js
@@ -3,12 +3,12 @@ import { connectToDatabase } from '@/lib/mongodb';
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-const corsHeaders = (req) => {
+const corsHeaders = (req) => ({
   "Access-Control-Allow-Origin": req.headers.get("origin"),
   "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
   "Access-Control-Allow-Credentials": "true",
-};
+});
 
 export async function OPTIONS(req) {
   return NextResponse.json({}, { headers: corsHeaders(req) });
