f0fab1c45decda3511d5b29ab4e85db31cdc8a9d
massylamarni <massyl.amarni@outlook.fr>
2025-09-21 17:25:32 +0200

    auth fix #2

--- src/app/api/get/verify/route.js
+++ src/app/api/get/verify/route.js
@@ -1,6 +1,7 @@
+import { NextResponse } from 'next/server';
 import jwt from "jsonwebtoken";
 
-export async function GET(req: Request) {
+export async function GET(req) {
   try {
     const authHeader = req.headers.get("authorization");
     const token = authHeader?.split(" ")[1] || req.cookies.get("auth_token")?.value;
@@ -15,3 +16,4 @@ export async function GET(req: Request) {
     return NextResponse.json({ error: "Invalid token" }, { status: 403 });
   }
 }
+
